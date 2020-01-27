d36286f8adc64e9c9f102ecda193127c59d7a715
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-01-27 19:35:25 +0100

    creates a box

--- js/game.js
+++ js/game.js
@@ -1,10 +1,25 @@
-class game{
-    constructor(options, callback){
-        this.ctx = options.ctx;
+class Game{
+    constructor(ctx){
+        this.ctx = ctx;
 
     }
 
   _drawSpaceShip(){
+    this.ctx.fillStyle = 'green';  
+    this.ctx.fillRect(50,350,50,50);
       
+      console.log('works')
   }   
+
+
+
+
+
+start(){
+    this._drawSpaceShip();
+};
+
+
+
+
 }
--- js/main.js
+++ js/main.js
@@ -7,13 +7,20 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     let music = document.getElementById('music');
     let logo = document.getElementById('logo');
 
+
+    ctx = canvas.getContext('2d');
+    game = new Game(ctx);
+
 function destroyStartScreen (){
     startBtn.style = 'display: none';
     music.pause();
     logo.style = 'display: none';
+    
+    
 }
 
 startBtn.addEventListener('click', function(){
     destroyStartScreen();
+    game.start();
 })
 })
